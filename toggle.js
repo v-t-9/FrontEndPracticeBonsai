2a3e650b4f100369ba8617c15fb9d4bc3d0e7b3a
v-t-9 <violeta.a.t.9@gmail.com>
2023-08-24 15:25:37 -0300

    toggle

---
+++ toggle.js
@@ -0,0 +1,35 @@
+function Toggle() {
+    let price = document.getElementsByClassName("price");
+    let descrip = document.getElementsByClassName("description")
+    initialText = "$17"
+        if (price[0].textContent.includes(initialText)) {
+            
+            price[0].textContent = "$24";
+            price[1].textContent = "$39";
+            price[2].textContent = "$79";
+            descrip[0].textContent = "Ideal for freelancers and contractors just starting out."
+            descrip[1].textContent = "Everything a growing independent business needs to thrive."
+            descrip[2].textContent = "The perfect package for small businesses and agencies."
+            document.getElementById("yearly").style.color = "var(--dark-green)";
+            document.getElementById("monthly").style.color = "var(--light-grey)";
+            document.documentElement.style.setProperty('--width', "100px")
+            
+        } else {
+
+            price[0].textContent = "$17";
+            price[1].textContent = "$32";
+            price[2].textContent = "$52";
+            descrip[0].textContent = "Ideal for freelancers and contractors just starting out. Billed annually."
+            descrip[1].textContent = "Everything a growing independent business needs to thrive Billed annually."
+            descrip[2].textContent = "The perfect package for small businesses and agencies. Billed annually."
+            document.getElementById("monthly").style.color = "var(--dark-green)";
+            document.getElementById("yearly").style.color = "var(--light-grey)";
+            document.documentElement.style.setProperty('--width', "210px")
+        }       
+}
+
+    
+    
+
+
+
